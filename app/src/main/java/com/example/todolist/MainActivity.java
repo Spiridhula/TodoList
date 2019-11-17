674df2e2ba0c4262a3d4b7cfa031bbee971c5ec9
Spiridhula <spiridhula.zguri@fshnstudent.info>
2019-11-17 19:55:45 +0100

    final changes

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
     ArrayList<String> tasks;
     ArrayAdapter<String> adapter;
 
@@ -24,56 +23,45 @@ public class MainActivity extends AppCompatActivity {
     Button addBtn;
     ListView listView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.items_list);
         addBtn = findViewById(R.id.btnAdd);
         itemText = findViewById(R.id.item_edit_text);
-
         tasks = new ArrayList<>();
-
         adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_multiple_choice, tasks);
 
 
         View.OnClickListener addListener = new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-
-                tasks.add(itemText.getText().toString());
-                itemText.setText("");
-                adapter.notifyDataSetChanged();
-
+                if (itemText.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Type something!", Toast.LENGTH_SHORT).show();
+                } else {
+                    tasks.add(itemText.getText().toString());
+                    itemText.setText("");
+                    adapter.notifyDataSetChanged();
+                }
             }
-
-
         };
-
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                SparseBooleanArray positionCheck= listView.getCheckedItemPositions();
-
-                int counter= listView.getCount();
-
-                for (int j = counter-1; j>=0;j--){
-                    if (positionCheck.get(j)){
+                SparseBooleanArray positionCheck = listView.getCheckedItemPositions();
+                int counter = listView.getCount();
+                for (int j = counter - 1; j >= 0; j--) {
+                    if (positionCheck.get(j)) {
                         adapter.remove(tasks.get(j));
-
-                        Toast.makeText(MainActivity.this,"Task deleted!",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Task deleted!", Toast.LENGTH_SHORT).show();
                     }
                 }
-
                 positionCheck.clear();
                 adapter.notifyDataSetChanged();
-
-
                 return false;
             }
         });
--- app/src/main/java/com/example/todolist/SplashScreen.java
+++ app/src/main/java/com/example/todolist/SplashScreen.java
@@ -1,8 +1,9 @@
 package com.example.todolist;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
+import android.view.Window;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +14,18 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splashscreen);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         //using a Handler to delay the main activity after splashscreen
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent (SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
 
             }
-        },2000);
+        }, 2000);
     }
 }
